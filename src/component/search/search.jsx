1276a412265826f362346def55f95d474021765b
hashemihossein <hashemihossein7899@gmail.com>
2024-05-19 08:50:05 +0330

    adding debounce effect for search input change

--- src/component/search/search.jsx
+++ src/component/search/search.jsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import * as styles from "./search.module.css";
+import { useDebounce } from "../../hooks";
 
 export const Search = () => {
-  return <div className={styles.container}></div>;
+  const [focused, setFocused] = useState(false);
+  const [searchedText, setSearchedText] = useState("");
+
+  const debouncedValue = useDebounce(searchedText, 1000);
+
+  const DSearch = useCallback(async () => {
+    const res = await fetch(
+      `https://demo.dataverse.org/api/search?q=${debouncedValue}`
+    );
+    const json = await res.json();
+    console.log(json);
+  }, [debouncedValue]);
+
+  useEffect(() => {
+    DSearch();
+  }, [debouncedValue, DSearch]);
+
+  const handleSearchTextChange = (event) => {
+    setSearchedText(event.target.value);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.inputContainer}
+        style={{
+          height: focused ? "100vh" : "3rem",
+        }}
+      >
+        <input
+          className={styles.input}
+          type="text"
+          onFocus={() => {
+            setFocused(true);
+          }}
+          onChange={handleSearchTextChange}
+        />
+      </div>
+    </div>
+  );
 };
---
+++ src/hooks/useDebounce/useDebounce.hook.js
@@ -0,0 +1,16 @@
+import { useState, useRef, useEffect } from "react";
+
+export const useDebounce = (value, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState("");
+  const timerRef = useRef();
+
+  useEffect(() => {
+    timerRef.current = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => {
+      clearTimeout(timerRef.current);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
