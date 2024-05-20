46d57dc0f6ae302212c6705dca5ca50fe70c47f7
hashemihossein <hashemihossein7899@gmail.com>
2024-05-20 14:22:26 +0330

    designing search list item component

--- src/component/search-list-item/searchListItem.jsx
+++ src/component/search-list-item/searchListItem.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import * as styles from "./searchListItem.module.css";
+import RoutingIcon from "./../../assets/RoutingIcon.svg";
 
 export const SearchListItem = (props) => {
   const { item, index } = props;
   return (
-    <li>
-      <div>item {index}</div>
+    <li className={styles.container}>
+      <div className={styles.rowContainer}>
+        <div>
+          <div className={styles.title}>{item?.title}</div>
+          <div className={styles.category}>{item?.category}</div>
+          <div className={styles.address}>{item?.address}</div>
+        </div>
+        <button className={styles.buttonContainer}>
+          <img width={"22px"} alt="RoutingIcon" src={RoutingIcon} />
+        </button>
+      </div>
+      <div className={styles.hr} />
     </li>
   );
 };
