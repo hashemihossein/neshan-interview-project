3147e5e4fb84ada23cf3d614d32de97022aea1f2
hashemihossein <hashemihossein7899@gmail.com>
2024-05-22 23:43:17 +0330

    adding error boundary to project

--- index.js
+++ index.js
@@ -7,15 +7,29 @@ import {
   ToastProvider,
   SearchContextProvider,
 } from "./src/context";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorIcon from "./src/assets/Error.png";
+
+function Fallback({ error }) {
+  return (
+    <div className="error-container">
+      <img src={ErrorIcon} width="100px" height="100px" />
+      <h1>خطایی رخ داده است!</h1>
+      <span>خواهشمندیم پس از حفظ صبوری خود کمی صبر کنید</span>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
-    <MapContextProvider>
-      <SearchContextProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      </SearchContextProvider>
-    </MapContextProvider>
+    <ErrorBoundary FallbackComponent={Fallback}>
+      <MapContextProvider>
+        <SearchContextProvider>
+          <ToastProvider>
+            <App />
+          </ToastProvider>
+        </SearchContextProvider>
+      </MapContextProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
